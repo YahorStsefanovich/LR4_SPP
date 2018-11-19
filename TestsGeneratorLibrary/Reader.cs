c691f2ccecdfdbba84ce5c7b782286f304cd4951
Yegor Stephanovich <stefanovich_egor@mail.ru>
2018-11-19 09:49:06 +0300

    Iinitital commit

---
+++ TestsGeneratorLibrary/Reader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+     public class Reader
+     {
+          public async Task<string> ReadAsync(string path)
+          {
+               using (StreamReader streamReader = new StreamReader(path))
+               {
+                    return await streamReader.ReadToEndAsync();
+               }
+          }
+     }
+}
---
+++ TestsGeneratorLibrary/Writer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+     public class Writer
+     {
+          private readonly string pathToOutputDirectory;
+
+          public Writer(string pathToOutputDirectory)
+          {
+               this.pathToOutputDirectory = pathToOutputDirectory;
+               if (!Directory.Exists(pathToOutputDirectory))
+               {
+                    Directory.CreateDirectory(pathToOutputDirectory);
+               }
+          }
+
+     }
+}
